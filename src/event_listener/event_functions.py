57f91ebb11ce2b35904d1e2ebbcd52fd2d095ace
sacha-renault <sacha35340@gmail.com>
2024-08-10 17:25:06 +0200

    wip: signature match add some new cehck on event_function._is_signature_match

--- src/event_listener/event_functions.py
+++ src/event_listener/event_functions.py
@@ -1,4 +1,4 @@
-from typing import Callable, Tuple, Any, List
+from typing import Callable, Tuple, Any, List, get_type_hints
 import inspect
 from inspect import Parameter, _empty
 
@@ -16,12 +16,30 @@ def _is_signature_match(base_signature: List[Parameter], callback_signature: Lis
         return True # bypass the type check
     
     # Else check for every parameter
-    for base_param, callback_param in zip(base_signature, callback_signature):
-        # Check if both 
-        if (base_param.default is _empty) != (callback_param.default is _empty):
+    # TODO zip makes the check on the shortest ... it can lead to errors ...
+    for base_param, callback_param in zip(base_signature, callback_signature): 
+        # Check if param are same kind
+        if base_param.kind == callback_param.kind:
+
+            # Check if both have a default value
+            if (base_param.default is _empty) != (callback_param.default is _empty):
+                return False
+
+            # Check if params type !
+            # TODO
+        else:
             return False
     return True
 
+def test_function(arg1 : int, arg2 : float, *args, papap: Callable, pipo: List = None, **kwargs):
+    pass
+
+def test_function2(arg1, arg2 : float, *args, papap: Callable, pipo: List = None):
+    pass
+
 if __name__ == "__main__":
-    for v in _get_signature(_is_signature_match):
-        print(v.default)
+    s1 = _get_signature(test_function)
+    s2 = _get_signature(test_function2)
+    print(_is_signature_match(s1, s2))
+
+
